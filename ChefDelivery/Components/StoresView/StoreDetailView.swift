39a191b5c78c20f60ff64dc310087994d8693a54
Alex Souza <alexjoseps@icloud.com>
2026-06-24 23:19:09 -0300

    chore: add lists of producs to store detail with scrollview

--- ChefDelivery/Components/StoresView/StoreDetailView.swift
+++ ChefDelivery/Components/StoresView/StoreDetailView.swift
@@ -11,45 +11,71 @@ struct StoreDetailView: View {
     let store: StoreType
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Image(store.headerImage)
-                .resizable()
-                .scaledToFit()
-            
-            HStack {
-                Text(store.name)
-                    .font(.title)
-                    .bold()
-                
-                Spacer()
-
-                Image(store.logoImage)
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .leading) {
+                Image(store.headerImage)
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 50, height: 50)
-            }
-            .padding(10)
-            
-            HStack {
-                Text(store.location)
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
                 
-                Spacer()
+                HStack {
+                    Text(store.name)
+                        .font(.title)
+                        .bold()
+                    
+                    Spacer()
+                    
+                    Image(store.logoImage)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                }
+                .padding(10)
                 
-                ForEach(1...store.stars, id: \.self) { _ in
+                HStack {
+                    Text(store.location)
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                    
+                    Spacer()
+                    
+                    ForEach(1...store.stars, id: \.self) { _ in
                         Image(systemName: "star.fill")
-                        .foregroundStyle(Color.yellow)
-                        .font(.caption)
+                            .foregroundStyle(Color.yellow)
+                            .font(.caption)
+                    }
+                }
+                .padding(10)
+                
+                Text("Produtos")
+                    .font(.title2)
+                    .padding(10)
+                
+                ForEach(store.products) { product in
+                    HStack {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text(product.name)
+                                .font(.headline)
+                            
+                            Text(product.description)
+                                .font(.subheadline)
+                                .foregroundStyle(Color.secondary)
+                            
+                            Text("R$ \(product.price, specifier: "%.2f")")
+                        }
+                        
+                        Spacer()
+                        
+                        Image(product.image)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(12)
+                            .frame(width: 120, height: 120)
+                    }
+                    .padding()
                 }
             }
-            .padding(10)
-            
-            Text("Produtos")
-                .font(.title2)
-                .padding(10)
+            .navigationTitle(store.name)
         }
-        .navigationTitle(store.name)
     }
 }
 
