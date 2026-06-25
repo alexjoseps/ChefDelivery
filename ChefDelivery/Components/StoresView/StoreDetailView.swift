26e8c9202b191eeb00e9e471d8168839f4c9f9c2
Alex Souza <alexjoseps@icloud.com>
2026-06-24 22:46:25 -0300

    chore: implement basic/header informations to store detail view

--- ChefDelivery/Components/StoresView/StoreDetailView.swift
+++ ChefDelivery/Components/StoresView/StoreDetailView.swift
@@ -11,7 +11,45 @@ struct StoreDetailView: View {
     let store: StoreType
 
     var body: some View {
-        Text(store.name)
+        VStack(alignment: .leading) {
+            Image(store.headerImage)
+                .resizable()
+                .scaledToFit()
+            
+            HStack {
+                Text(store.name)
+                    .font(.title)
+                    .bold()
+                
+                Spacer()
+
+                Image(store.logoImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 50)
+            }
+            .padding(10)
+            
+            HStack {
+                Text(store.location)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                Spacer()
+                
+                ForEach(1...store.stars, id: \.self) { _ in
+                        Image(systemName: "star.fill")
+                        .foregroundStyle(Color.yellow)
+                        .font(.caption)
+                }
+            }
+            .padding(10)
+            
+            Text("Produtos")
+                .font(.title2)
+                .padding(10)
+        }
+        .navigationTitle(store.name)
     }
 }
 
